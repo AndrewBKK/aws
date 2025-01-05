3798ade768a00010fa8169e64b1690a7e68c1807
AndrewBKK <andrew@cmslab.co>
2025-01-05 09:11:36 +0000

    Fixing main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,3 @@
 provider "aws" {
   region = var.aws_region
 }
-
-module "s3" {
-  source = "./s3.tf"
-}
-
-module "lambda" {
-  source = "./lambda.tf"
-}
-
-module "api_gateway" {
-  source = "./api_gateway.tf"
-}
-
-module "cloudfront" {
-  source = "./cloudfront.tf"
-}
