e385ba9a1f487a4fe59933b7a020f4fb026bdde7
AndrewBKK <andrew@cmslab.co>
2025-01-05 09:59:03 +0000

    Fixing s3

--- s3.tf
+++ s3.tf
@@ -2,6 +2,14 @@ resource "aws_s3_bucket" "static_assets" {
   bucket = var.s3_bucket_name
 }
 
+resource "aws_s3_bucket_public_access_block" "static_assets" {
+  bucket                  = aws_s3_bucket.static_assets.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_object" "server_functions" {
   bucket = aws_s3_bucket.static_assets.id
   key    = "server-functions.zip"
@@ -12,19 +20,4 @@ resource "aws_s3_object" "image_optimization" {
   bucket = aws_s3_bucket.static_assets.id
   key    = "image-optimization.zip"
   source = "./image-optimization.zip"
-}
-
-resource "aws_s3_bucket_policy" "static_assets_policy" {
-  bucket = aws_s3_bucket.static_assets.id
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect    = "Allow",
-        Principal = "*",
-        Action    = ["s3:GetObject"],
-        Resource  = ["${aws_s3_bucket.static_assets.arn}/*"]
-      }
-    ]
-  })
 }
